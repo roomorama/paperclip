c92d277e39eb2ec814038a8bbd7b30cb4db9e2db
Jon Yurek <jyurek@thoughtbot.com>
2014-01-24 14:36:35 -0500

    WIP

--- lib/paperclip/io_adapters/data_uri_adapter.rb
+++ lib/paperclip/io_adapters/data_uri_adapter.rb
@@ -12,11 +12,16 @@ module Paperclip
     private
 
     def cache_current_values
-      self.original_filename = 'base64.txt'
       data_uri_parts ||= @target_uri.match(REGEXP) || []
-      @content_type = data_uri_parts[1] || 'text/plain'
+      @content_type = ContentTypeDetector.new(@tempfile.path)
+      self.original_filename = "data.#{extension_for(@content_type)}"
       @target = StringIO.new(Base64.decode64(data_uri_parts[2] || ''))
       @size = @target.size
+    end
+
+    def extension_for(content_type)
+      type = MIME::Types[content_type].first
+      type && type.extensions.first
     end
 
   end
